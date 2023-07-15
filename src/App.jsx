d1e1843b4ffe7c35964140a743703f9eda92bd30
DarioCostaAlves <dario.c.alves22@gmail.com>
2023-07-15 17:00:22 +0100

    builded a static version

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,80 @@
 import "./App.css";
 import data from "./component/data/products.json";
 
-function App() {
+function ProductCategoryRow({ category }) {
   return (
-    <>
-      <h1>Dario Minimarket</h1>
-    </>
+    <tr>
+      <th colSpan="2">{category}</th>
+    </tr>
   );
+}
+
+function ProductRow({ product }) {
+  const name = product.stocked ? (
+    product.name
+  ) : (
+    <span style={{ color: "red" }}>{product.name}</span>
+  );
+  return (
+    <tr>
+      <td>{name}</td>
+      <td>{product.price}</td>
+    </tr>
+  );
+}
+
+function ProductTable({ products }) {
+  const rows = [];
+  let lastCategory = null;
+
+  products.forEach((product) => {
+    if (product.category !== lastCategory) {
+      rows.push(
+        <ProductCategoryRow
+          category={product.category}
+          key={product.category}
+        />
+      );
+    }
+    rows.push(<ProductRow product={product} key={product.name} />);
+    lastCategory = product.category;
+  });
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Nome</th>
+          <th>Preço</th>
+        </tr>
+      </thead>
+      <tbody>{rows}</tbody>
+    </table>
+  );
+}
+
+function SearchBar() {
+  return (
+    <form>
+      <input type="text" placeholder="Search..." />
+      <label>
+        <input type="checkbox" /> Apenas exibir produtos em stock
+      </label>
+    </form>
+  );
+}
+
+function FilterableProductTable({ products }) {
+  return (
+    <div>
+      <SearchBar />
+      <ProductTable products={products} />
+    </div>
+  );
+}
+
+function App() {
+  return <FilterableProductTable products={data} />;
 }
 
 export default App;
